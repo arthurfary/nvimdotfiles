8926e5ea4b41bf636f83a17e05e07cf8f1d06f41
arthurfary <arthurfary22@gmail.com>
2024-10-04 15:55:14 -0300

    surround + clang

---
+++ config/keymaps.lua
@@ -0,0 +1,20 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- mode, key, command, extra
+vim.keymap.set("n", "<C-t>", ":ToggleTerm <CR>", { desc = "Toggle Terminal" })
+vim.keymap.set("n", "<C-s>", ":w <CR>", { desc = "Save current buffer" })
+
+-- rename
+vim.keymap.set("n", "<leader>cr", ":IncRename ")
+
+-- remove ^M
+vim.keymap.set("n", "<leader>cc", ":%s/\r$//g <CR>")
+
+-- delete without yanking, blackhole
+vim.keymap.set("v", "d", '"_d')
+vim.keymap.set("n", "dd", '"_dd')
+
+-- neotree
+vim.keymap.set("n", "<leader>e", ":Neotree toggle position=right <CR>")
